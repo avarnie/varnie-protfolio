031486b0843a41a8a33fc6225c19297ce594741b
avarnie <varnie231@gmail.com>
2022-11-19 19:43:29 +0000

    Initail commit

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,62 +1,110 @@
 import React from "react";
-// import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 
 export default function Contact() {
     return (
         <>
-            {/* <Navbar />
-            <div>
+            <Navbar />
+            {/* <div>
                 <p>Hello There</p>
             </div> */}
 
-<div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-    {/* <!--Card 1--> */}
-    <div class="rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="/mountain.jpghttps://picsum.photos/100" alt="Mountain" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Mountain</div>
-        <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-      </div>
-    </div>
-    {/* <!--Card 2--> */}
-    <div class="rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="/river.jpg" alt="River" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">River</div>
-        <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#summer</span>
-      </div>
-    </div>
+{/* <!-- Container for demo purpose --> */}
+<div class="container my-24 px-6 mx-auto">
 
-    {/* <!--Card 3--> */}
-    <div class="rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="/forest.jpg" alt="Forest" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Forest</div>
-        <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#fall</span>
-      </div>
+  {/* <!-- Section: Design Block --> */}
+  <section class="mb-32 text-center text-gray-800">
+    <div class="max-w-[700px] mx-auto px-3 lg:px-6">
+      <h2 class="text-3xl font-bold mb-12">Contact us</h2>
+      <form>
+        <div class="form-group mb-6">
+          <label class="text-xl font-bold mb-12">Name</label><br />
+          <input type="text" class="form-control block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
+            placeholder="Name" />
+        </div>
+        <div class="form-group mb-6">
+          <label class="text-xl font-bold mb-12">Email</label><br />
+          <input type="email" class="form-control block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
+            placeholder="Email address" />
+        </div>
+        <div class="form-group mb-6">
+          <label class="text-xl font-bold mb-12">Message</label><br />
+          <textarea class="
+            form-control
+            block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+          " id="exampleFormControlTextarea13" rows="3" placeholder="Message"></textarea>
+        </div>
+        <div class="form-group form-check text-center mb-6">
+          <input type="checkbox"
+            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+            id="exampleCheck87" check />
+          <label class="form-check-label inline-block text-gray-800" for="exampleCheck87">Send me a copy of this
+            message</label>
+        </div>
+        <button type="submit" class="
+          w-full
+          px-6
+          py-2.5
+          bg-blue-600
+          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-blue-700 hover:shadow-lg
+          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-blue-800 active:shadow-lg
+          transition
+          duration-150
+          ease-in-out">Send</button>
+      </form>
     </div>
-  </div>
+  </section>
+  {/* <!-- Section: Design Block --> */}
+
+</div>
+{/* <!-- Container for demo purpose --> */}
         </>
     );
 }
